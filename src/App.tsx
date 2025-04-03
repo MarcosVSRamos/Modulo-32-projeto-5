b9f088be16a6603fb51f9e664ceeecb0a6c1f524
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-04-03 12:54:54 -0400

    configurando o estado do redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import EstiloGlobal, { Container } from './styles'
 import BarraLateral from './Containers/BarraLateral'
 import ListaDeTarefas from './Containers/ListaDeTarefas'
+import { Provider } from 'react-redux'
+import store from './store'
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <EstiloGlobal />
       <Container>
         <BarraLateral />
         <ListaDeTarefas />
       </Container>
-    </>
+    </Provider>
   )
 }
 
