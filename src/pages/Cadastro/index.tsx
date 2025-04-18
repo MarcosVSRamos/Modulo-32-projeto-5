f65743a692ffe0a7bf2359e1b7b70bb1934e1a71
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-04-18 09:28:35 -0400

    iniciando a criação de tarefas

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,3 +1,15 @@
-const Cadastro = () => <div>nova tarefa</div>
+const Cadastro = () => (
+  <form>
+    <input type="text" placeholder="Título" />
+    <textarea placeholder="Descrição da Tarefa" />
+    <input name="prioridade" type="radio" id="urgente" />
+    <label htmlFor="urgente">Urgente</label>
+    <input name="prioridade" type="radio" id="importante" />
+    <label htmlFor="importante">Iportante</label>
+    <input name="prioridade" type="radio" id="normal" />
+    <label htmlFor="normal">Normal</label>
+    <button type="submit">Cadastrar</button>
+  </form>
+)
 
 export default Cadastro
