63053b3cc44f6fb23503fff506a00dcc7fe428a2
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-04-18 09:03:18 -0400

    criando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import EstiloGlobal, { Container } from './styles'
-import BarraLateral from './Containers/BarraLateral'
-import ListaDeTarefas from './Containers/ListaDeTarefas'
 import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import EstiloGlobal, { Container } from './styles'
+
 import store from './store'
+import Home from './pages/Home'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <h1>Nova tarefa</h1>
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
