79b8139cc5879a92870033cb712d344b570b31d0
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-04-28 20:14:27 -0400

    adicionando checkbox no titulo das tarefas

--- src/components/Tarefa/styles.ts
+++ src/components/Tarefa/styles.ts
@@ -26,12 +26,18 @@ export const Card = styled.div`
   padding: 16px;
   margin-bottom: 32px;
   border-radius: 16px;
+
+  label {
+    display: flex;
+    margin-bottom: 16px;
+    align-items: center;
+  }
 `
 
 export const Titulo = styled.h3`
   font-size: 18px;
   font-weight: bold;
-  margin-bottom: 16px;
+  margin-left: 8px;
 `
 
 export const Tag = styled.span<TagProps>`
